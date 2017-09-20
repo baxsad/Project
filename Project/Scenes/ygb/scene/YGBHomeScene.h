0c33915ed13587350cbf353615d447701b01ae77
pmo <pmo@pmodeiMac.lan>
2017-09-19 17:46:10 -0700

    no message

---
+++ Project/Scenes/ygb/scene/YGBHomeScene.h
@@ -0,0 +1,13 @@
+//
+//  YGBHomeScene.h
+//  Project
+//
+//  Created by jearoc on 2017/9/19.
+//  Copyright © 2017年 jearoc. All rights reserved.
+//
+
+#import "UIScene.h"
+
+@interface YGBHomeScene : UIScene
+
+@end
---
+++ Project/Scenes/ygb/scene/YGBHomeScene.m
@@ -0,0 +1,30 @@
+//
+//  YGBHomeScene.m
+//  Project
+//
+//  Created by jearoc on 2017/9/19.
+//  Copyright © 2017年 jearoc. All rights reserved.
+//
+
+#import "YGBHomeScene.h"
+
+@interface YGBHomeScene ()
+
+@end
+
+@implementation YGBHomeScene
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.statusBarStyle = UIStatusBarStyleLightContent;
+  self.navBarBarTintColor = [UIColor blackColor];
+  self.view.backgroundColor = [UIColor blackColor];
+  
+  
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+}
+
+@end
