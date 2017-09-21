ccd87f86b6ee974ef30a39f49eaef6546624e2b9
wangrui <wr@sunako.org>
2017-09-21 20:46:03 +0800

    no message

---
+++ Project/Common/BaseUI/UITableScene.h
@@ -0,0 +1,13 @@
+//
+//  UITableScene.h
+//  Project
+//
+//  Created by jearoc on 2017/9/21.
+//  Copyright © 2017年 jearoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableScene : UITableView
+
+@end
---
+++ Project/Common/BaseUI/UITableScene.m
@@ -0,0 +1,21 @@
+//
+//  UITableScene.m
+//  Project
+//
+//  Created by jearoc on 2017/9/21.
+//  Copyright © 2017年 jearoc. All rights reserved.
+//
+
+#import "UITableScene.h"
+
+@implementation UITableScene
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
